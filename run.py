1538339e7f9c9d4225a0228b60d6eeed8fcf8d17
emerson <guedes.emerson@hotmail.com>
2019-08-14 22:29:53 -0300

    Ajustando fields

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
+import os
 from flask_api import app
 from flask_api.product import views
 
-if __name__ == "__main__":
-    app.run(debug=True, port = 5000)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 
